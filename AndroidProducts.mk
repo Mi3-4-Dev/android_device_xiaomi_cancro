Ratoriku/device_xiaomi_cancro

cancro: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/havoc_cancro.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_cancro-user \
+    havoc_cancro-userdebug \
+    havoc_cancro-eng

Mi3-4-Dev/android_device_xiaomi_cancro

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_cancro.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_cancro-user \
+    lineage_cancro-userdebug \
+    lineage_cancro-eng