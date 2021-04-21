22e666d8c1abe8994f4572034a4252d68a222e03
TARKZiM <tom8476oo@gmail.com>
2021-04-21 19:48:01 +0800

    cancro: init: Update build fingerprint to match latest MIUI Global Stable
    * V9.5.7.0.MXDMIFA

--- init/init_cancro.cpp
+++ init/init_cancro.cpp
@@ -127,8 +127,8 @@ void vendor_load_properties()
 
     property_override_dual("ro.product.device", "ro.vendor.product.device", "cancro");
     property_override_dual("ro.product.name", "ro.vendor.product.name", "cancro");
-    property_override("ro.build.fingerprint", "Xiaomi/cancro/cancro:6.0.1/MMB29M/V8.1.6.0.MXDMIDI:user/release-keys");
-    property_override("ro.build.description", "cancro-user 6.0.1 MMB29M V8.1.6.0.MXDMIDI release-keys");
+    property_override("ro.build.fingerprint", "Xiaomi/cancro/cancro:6.0.1/MMB29M/V9.5.7.0.MXDMIFA:user/release-keys");
+    property_override("ro.build.description", "cancro-user 6.0.1 MMB29M V9.5.7.0.MXDMIFA release-keys");
 
     switch (raw_id) {
         case 1978:
